f3f9c824d4e7c0f5a1c5267e495b0ff32f01db5e
Vikash-GitHere <vitan2022@gmail.com>
2024-02-12 01:19:16 +0530

    link

--- app/page.js
+++ app/page.js
@@ -99,7 +99,7 @@ export default function Home() {
               <a
                 className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 target="_blank"
-                href="https://tax-main.vercel.app/"
+                href="https://taxy-calculator.vercel.app/"
               >
                 Go To Taxy
                 <svg
